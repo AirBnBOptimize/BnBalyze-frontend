3a79f6eaa6bcbe15faa4b2d38b844954618b271d
Red <vhanchar@ucsc.edu>
2019-08-26 15:21:06 -0700

    added initial login component, needs to be connected to welcome page

--- optimizer/src/App.js
+++ optimizer/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import './components/login.css';
+
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
       <h1>BnBalyze</h1>
+      <Login />
     </div>
   );
 }
