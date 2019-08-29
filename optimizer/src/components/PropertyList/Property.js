9b968346a1b1775855116552825cbcea01790395
etridgely <5757423+etridgely@users.noreply.github.com>
2019-08-29 11:42:50 -0700

    🚀 Deploying stuff

--- optimizer/src/components/PropertyList/Property.js
+++ optimizer/src/components/PropertyList/Property.js
@@ -10,16 +10,19 @@ const Property = (props) => {
     <Card className="image">
     <Image src="" wrapped ui={false}/>
     <Card.Content className="property-card">
+      
       <Card.Header>{props.name}</Card.Header>
       <Card.Description className="room_type">{props.room_type} · {props.accommodates} Beds </Card.Description>
       <Card.Description className="neighborhood">{props.neighborhood} </Card.Description>
       <Card.Description>Bedrooms · {props.bedrooms}</Card.Description>
+
       <div className="amenities">
       <Card.Description><FontAwesomeIcon icon={faWifi} className="icon-wifi" /> {props.wifi}</Card.Description>
       <Card.Description><FontAwesomeIcon icon={faTv} className="icon-tv" /> {props.cable_tv}</Card.Description>
       <Card.Description><FontAwesomeIcon icon={faTshirt} className="icon-dryer" /> {props.washer}</Card.Description>
       <Card.Description><FontAwesomeIcon icon={faUtensils} className="icon-kitchen" /> {props.kitchen}</Card.Description>
       </div>
+
       <div className="pricecompare">
         <div className="pricebox-container">
           <h6>Current</h6>
@@ -30,6 +33,7 @@ const Property = (props) => {
           <div className="pricebox"><p className="price">$52 / night</p></div>
         </div>
       </div>
+
     </Card.Content>
     <Card.Content className="property-card" extra>
       <a>
