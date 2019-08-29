936e44f2cad645f58034c8f315eb621a95715ccf
etridgely <5757423+etridgely@users.noreply.github.com>
2019-08-29 08:15:44 -0700

    🎨 Improving structure / format of the code.

--- optimizer/src/components/PropertyList/Property.js
+++ optimizer/src/components/PropertyList/Property.js
@@ -1,22 +1,22 @@
 import React from 'react'
 import { Card, Icon, Image } from 'semantic-ui-react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { fa-wifi } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faWifi, faTv, faUtensils, faTshirt } from '@fortawesome/free-solid-svg-icons';
 
 const Property = (props) => {
     return (
-    <Card>
-    <Image src="" wrapped ui={false} className="image" />
+    <Card className="image">
+    <Image src="" wrapped ui={false}/>
     <Card.Content className="property-card">
       <Card.Header>{props.name}</Card.Header>
       <Card.Description>{props.room_type} · {props.accommodates} Beds </Card.Description>
       <Card.Description>{props.neighborhood} </Card.Description>
       <Card.Description>Bedrooms · {props.bedrooms}</Card.Description>
       <div className="amenities">
-      <Card.Description>Wifi: {props.wifi}</Card.Description>
-      <Card.Description>Cable TV: {props.cable_tv}</Card.Description>
-      <Card.Description>Washer: {props.washer}</Card.Description>
-      <Card.Description>Kitchen: {props.kitchen}</Card.Description>
+      <Card.Description><FontAwesomeIcon icon={faWifi} className="icon-wifi" /> {props.wifi}</Card.Description>
+      <Card.Description><FontAwesomeIcon icon={faTv} className="icon-tv" /> {props.cable_tv}</Card.Description>
+      <Card.Description><FontAwesomeIcon icon={faTshirt} className="icon-dryer" /> {props.washer}</Card.Description>
+      <Card.Description><FontAwesomeIcon icon={faUtensils} className="icon-kitchen" /> {props.kitchen}</Card.Description>
       </div>
       <div className="pricecompare">
         <div className="pricebox"><p className="title">Current</p></div>
