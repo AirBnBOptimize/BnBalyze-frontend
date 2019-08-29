8df9683b1bb2a57deb564ba9eb454e478bce334f
etridgely <5757423+etridgely@users.noreply.github.com>
2019-08-29 09:57:40 -0700

    🎨 Improving structure / format of the code.

--- optimizer/src/components/PropertyList/Property.js
+++ optimizer/src/components/PropertyList/Property.js
@@ -3,14 +3,16 @@ import { Card, Icon, Image } from 'semantic-ui-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faWifi, faTv, faUtensils, faTshirt } from '@fortawesome/free-solid-svg-icons';
 
+
+
 const Property = (props) => {
     return (
     <Card className="image">
     <Image src="" wrapped ui={false}/>
     <Card.Content className="property-card">
       <Card.Header>{props.name}</Card.Header>
-      <Card.Description>{props.room_type} · {props.accommodates} Beds </Card.Description>
-      <Card.Description>{props.neighborhood} </Card.Description>
+      <Card.Description className="room_type">{props.room_type} · {props.accommodates} Beds </Card.Description>
+      <Card.Description className="neighborhood">{props.neighborhood} </Card.Description>
       <Card.Description>Bedrooms · {props.bedrooms}</Card.Description>
       <div className="amenities">
       <Card.Description><FontAwesomeIcon icon={faWifi} className="icon-wifi" /> {props.wifi}</Card.Description>
@@ -19,8 +21,14 @@ const Property = (props) => {
       <Card.Description><FontAwesomeIcon icon={faUtensils} className="icon-kitchen" /> {props.kitchen}</Card.Description>
       </div>
       <div className="pricecompare">
-        <div className="pricebox"><p className="title">Current</p></div>
-        <div className="pricebox"><p className="title">Recommendation</p></div>
+        <div className="pricebox-container">
+          <h6>Current</h6>
+          <div className="pricebox"><p className="price">$34 / night</p></div>
+        </div>
+        <div className="pricebox-container">
+          <h6>Recommendation</h6>
+          <div className="pricebox"><p className="price">$52 / night</p></div>
+        </div>
       </div>
     </Card.Content>
     <Card.Content className="property-card" extra>
