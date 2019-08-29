a7fa7c3477e76447bd56fe622ad0584505986fdc
etridgely <5757423+etridgely@users.noreply.github.com>
2019-08-29 08:47:42 -0700

    quick patch

--- optimizer/src/components/propertyList/PropertyList.js
+++ optimizer/src/components/propertyList/PropertyList.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Property from "./Property";
+import "./PropertyList.scss";
+import { Container } from 'semantic-ui-react'
 
-const PropertyList = () => {
-    return ( <h2>Property List</h2> );
-}
+export default function PropertyList() {
+  const [property, setProperty] = useState([]);
  
-export default PropertyList;
+  useEffect(() => {
+       axios
+        .get(`https://bnbalyze.herokuapp.com/properties`, {headers: {Authorization: localStorage.getItem('token')}})
+        .then(response => {
+          setProperty(response.data);
+          console.log(response.data);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+
+  },[]);
+
+  return (
+    <section className="property-list-grid-view">
+      <Container className="container">
+          {property.map(id => (   
+            <div className="cardStyle">
+            <Property
+              key={id}
+              image={id.image}
+              neighborhood={id.neighborhood}
+              room_type={id.room_type}
+              accommodates={id.accommodates}
+              bedrooms={id.bedrooms}
+              reviews={id.number_of_reviews}
+              wifi={id.wifi}
+              cable_tv={id.cable_tv}
+              washer={id.washer}
+              kitchen={id.kitchen}
+            />
+            </div>
+            ))}
+      </Container>
+    </section>
+  );
+}
